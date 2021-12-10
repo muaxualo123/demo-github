109487ea047c9eb3fc3c022d9835d79a159ba085
muaxualo123 <hung01645167156@gmail.com>
2021-12-10 12:11:30 +0700

    Create Bt2_Ch5.cs

---
+++ Bt2_Ch5.cs
@@ -0,0 +1,32 @@
+﻿/*
+2.	Viết chương trình nhập vào một số nguyên dương rồi in ra tất cả các ước số của số đó.
+đỗ phan duy hùng 
+cd21tt11
+
+**/
+using System;
+
+
+namespace CH5_BT2
+{
+    class Bt2_Ch5
+    {
+        static void Main(string[] args)
+        {
+            int uocSo = 0;
+            int n = 0;
+            int a = 0;
+            Console.WriteLine("nhap so nguyen bat ki n = ");
+            int.TryParse(Console.ReadLine(), out n);
+            for(a=1;a<= n;a++)
+            {if (n % a == 0)
+
+                {
+                   //onsole.Write();
+                    Console.Write(a + " : ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
